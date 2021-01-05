4f290f74715b7ab1a939cfe5b862197cd4e4d3a5
YanAmorelli <1404yan@gmail.com>
2021-01-05 18:46:49 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -11,6 +11,7 @@ namespace SalesWebMvc.Models
         public DateTime BirthDate { get; set; }
         public double BasicSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
@@ -42,6 +43,8 @@ namespace SalesWebMvc.Models
             return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).
                 Sum(sr => sr.Amount);
         }
+
+
     }
 
 }
